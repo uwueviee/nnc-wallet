ae2eaf5baaf77036457b925e438f3d050d485866
Evie Viau <evie@uwueviee.live>
2021-10-24 15:32:08 -0400

    rthsn

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,22 +1,72 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import {
+    IonButton,
+    IonButtons,
+    IonCard,
+    IonCardHeader,
+    IonCardSubtitle, IonCardTitle,
+    IonContent, IonFab, IonFabButton,
+    IonHeader, IonIcon, IonItem, IonLabel,
+    IonPage,
+    IonTitle,
+    IonToolbar
+} from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
+import {add, arrowForwardOutline, ellipsisHorizontal, ellipsisVertical, personCircle, search} from "ionicons/icons";
+
+const centerAmountStyle = {
+    marginTop: '10px'
+}
 
 const Tab1: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
+        <IonHeader>
+            <IonToolbar>
+                <IonTitle>Wallets</IonTitle>
+            </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+      <IonContent fullscreen>
+          <IonHeader collapse="condense">
+              <IonToolbar>
+                  <IonTitle size="large">Wallets</IonTitle>
+              </IonToolbar>
+          </IonHeader>
+
+          <div className="ion-text-center">
+              <h5>Total Balance</h5>
+              <h1 style={centerAmountStyle}>999 NNC</h1>
+          </div>
+
+          <hr />
+
+          <IonCard button>
+              <IonCardHeader>
+                  <IonCardSubtitle>
+                      Example Server 1
+                  </IonCardSubtitle>
+                  <IonCardTitle>
+                      432 NNC
+                  </IonCardTitle>
+              </IonCardHeader>
+          </IonCard>
+          <IonCard button>
+              <IonCardHeader>
+                  <IonCardSubtitle>
+                      Example Server 2
+                  </IonCardSubtitle>
+                  <IonCardTitle>
+                      567 COC
+                  </IonCardTitle>
+              </IonCardHeader>
+          </IonCard>
+
+          <IonFab vertical="bottom" horizontal="end" slot="fixed">
+              <IonFabButton>
+                  <IonIcon icon={add} />
+              </IonFabButton>
+          </IonFab>
+
       </IonContent>
     </IonPage>
   );
--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,4 +1,13 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import {
+    IonCard,
+    IonCardHeader,
+    IonCardSubtitle, IonCardTitle,
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar
+} from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
 
@@ -7,16 +16,95 @@ const Tab2: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>Transactions</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
+            <IonTitle size="large">Transactions</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+          <IonCard button>
+              <IonCardHeader>
+                  <IonCardSubtitle>
+                      Received
+                  </IonCardSubtitle>
+                  <IonCardTitle>
+                      67 NNC
+                  </IonCardTitle>
+                  <IonCardSubtitle>
+                      From: ec9b2332-a929-4069-9a50-87554384ca25
+                  </IonCardSubtitle>
+                  <IonCardSubtitle>
+                      To: Example Server 1
+                  </IonCardSubtitle>
+              </IonCardHeader>
+          </IonCard>
+          <IonCard button>
+              <IonCardHeader>
+                  <IonCardSubtitle>
+                      Sent
+                  </IonCardSubtitle>
+                  <IonCardTitle>
+                      12 NNC
+                  </IonCardTitle>
+                  <IonCardSubtitle>
+                      To: ec9b2332-a929-4069-9a50-87554384ca25
+                  </IonCardSubtitle>
+                  <IonCardSubtitle>
+                      From: Example Server 1
+                  </IonCardSubtitle>
+              </IonCardHeader>
+          </IonCard>
+          <IonCard button>
+              <IonCardHeader>
+                  <IonCardSubtitle>
+                      Received
+                  </IonCardSubtitle>
+                  <IonCardTitle>
+                      6 NNC
+                  </IonCardTitle>
+                  <IonCardSubtitle>
+                      From: ec9b2332-a929-4069-9a50-87554384ca25
+                  </IonCardSubtitle>
+                  <IonCardSubtitle>
+                      To: Example Server 1
+                  </IonCardSubtitle>
+              </IonCardHeader>
+          </IonCard>
+          <IonCard button>
+              <IonCardHeader>
+                  <IonCardSubtitle>
+                      Received
+                  </IonCardSubtitle>
+                  <IonCardTitle>
+                      12 NNC
+                  </IonCardTitle>
+                  <IonCardSubtitle>
+                      From: ec9b2332-a929-4069-9a50-87554384ca25
+                  </IonCardSubtitle>
+                  <IonCardSubtitle>
+                      To: Example Server 1
+                  </IonCardSubtitle>
+              </IonCardHeader>
+          </IonCard>
+          <IonCard button>
+              <IonCardHeader>
+                  <IonCardSubtitle>
+                      Sent
+                  </IonCardSubtitle>
+                  <IonCardTitle>
+                      654 NNC
+                  </IonCardTitle>
+                  <IonCardSubtitle>
+                      To: 941b37c7-7cc7-40a6-9613-bb3ca43bf70e
+                  </IonCardSubtitle>
+                  <IonCardSubtitle>
+                      From: Example Server 2
+                  </IonCardSubtitle>
+              </IonCardHeader>
+          </IonCard>
       </IonContent>
     </IonPage>
   );
