8587bede1a1b8bc88ce51b166f44374ed7d0a3ae
Evie Viau <evie@uwueviee.live>
2021-10-27 01:50:39 -0400

    cock

--- src/pages/PairWalletModal.tsx
+++ src/pages/PairWalletModal.tsx
@@ -1,19 +1,27 @@
 import {
-    IonButton, IonHeader, IonTitle, IonToolbar
+    IonButton, IonHeader, IonInput, IonItem, IonTitle, IonToolbar
 } from '@ionic/react';
+import {useState} from "react";
 
 const PairWalletModal: React.FC<{
     onDismiss: () => void;
-}> = ({ onDismiss }) => (
-    <div>
-        <h1>Pair Wallet</h1>
-        <IonButton expand="block" onClick={() => onDismiss()}>
-            Scan QR Code
-        </IonButton>
-        <IonButton expand="block" onClick={() => onDismiss()}>
-            Cancel
-        </IonButton>
-    </div>
-);
+}> = ({ onDismiss }) => {
+    const [text, setText] = useState<string>();
+
+    return(
+        <div>
+            <h1>Pair Wallet</h1>
+            <IonItem>
+                <IonInput value={text} placeholder="Enter Pairing Key" onIonChange={e => setText(e.detail.value!)} clearInput />
+            </IonItem>
+            <IonButton expand="block" onClick={() => onDismiss()}>
+                Add Wallet
+            </IonButton>
+            <IonButton expand="block" onClick={() => onDismiss()}>
+                Cancel
+            </IonButton>
+        </div>
+    );
+}
 
 export default PairWalletModal;
